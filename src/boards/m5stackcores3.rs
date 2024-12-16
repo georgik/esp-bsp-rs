45cfafea599d04370ff3b2145f1d3a0e6995cd3c
Juraj Michalek <juraj.michalek@espressif.com>
2024-12-16 19:39:57 +0100

    add M5Stack CoreS3

--- src/boards/m5stackcores3.rs
+++ src/boards/m5stackcores3.rs
@@ -8,9 +8,9 @@ macro_rules! lcd_spi {
                 ..esp_hal::spi::master::Config::default()
             },
         )
-        .with_sck($peripherals.GPIO7)
-        .with_mosi($peripherals.GPIO6)
-        .with_cs($peripherals.GPIO5)
+        .with_sck($peripherals.GPIO36)
+        .with_mosi($peripherals.GPIO37)
+        .with_cs($peripherals.GPIO3)
         .with_dma($dma_channel.configure(false, DmaPriority::Priority0))
     };
 }
@@ -30,9 +30,9 @@ macro_rules! lcd_dma_spi {
                 ..esp_hal::spi::master::Config::default()
             },
         )
-        .with_sck($peripherals.GPIO7)
-        .with_mosi($peripherals.GPIO6)
-        .with_cs($peripherals.GPIO5)
+        .with_sck($peripherals.GPIO36)
+        .with_mosi($peripherals.GPIO37)
+        .with_cs($peripherals.GPIO3)
         .with_dma(dma_channel.configure(false, DmaPriority::Priority0))
     }};
 }
@@ -40,7 +40,7 @@ macro_rules! lcd_dma_spi {
 #[macro_export]
 macro_rules! lcd_display_interface {
     ($peripherals:ident, $spi:expr) => {{
-        let lcd_dc = Output::new($peripherals.GPIO4, Level::Low);
+        let lcd_dc = Output::new($peripherals.GPIO35, Level::Low);
         display_interface_spi_dma::new_no_cs(LCD_MEMORY_SIZE, $spi, lcd_dc)
     }};
 }
@@ -48,14 +48,14 @@ macro_rules! lcd_display_interface {
 #[macro_export]
 macro_rules! lcd_reset_pin {
     ($peripherals:ident) => {
-        Output::new($peripherals.GPIO48, Level::Low)
+        Output::new($peripherals.GPIO15, Level::Low)
     };
 }
 
 #[macro_export]
 macro_rules! lcd_backlight_init {
     ($peripherals:ident) => {{
-        let mut backlight = Output::new($peripherals.GPIO45, Level::Low);
+        let mut backlight = Output::new($peripherals.GPIO0, Level::Low);
         backlight.set_high();
         Some(backlight)
     }};
@@ -64,23 +64,25 @@ macro_rules! lcd_backlight_init {
 #[macro_export]
 macro_rules! i2c_init {
     ($peripherals:ident) => {{
+        let i2c_sda = $peripherals.GPIO8;
+        let i2c_scl = $peripherals.GPIO18;
         I2c::new($peripherals.I2C0, esp_hal::i2c::master::Config::default())
-            .with_sda($peripherals.GPIO12)
-            .with_scl($peripherals.GPIO11)
+            .with_sda(i2c_sda)
+            .with_scl(i2c_scl)
     }};
 }
 
 #[macro_export]
 macro_rules! lcd_display {
-    ($peripherals:ident, $di:expr, $orientation:expr) => {{
-        mipidsi::Builder::new(mipidsi::models::ILI9341Rgb565, $di)
-            .display_size(240, 320)
+    ($peripherals:ident, $di:expr) => {{
+        mipidsi::Builder::new(mipidsi::models::ILI9342CRgb565, $di)
+            .display_size(320, 240)
             .orientation(
                 mipidsi::options::Orientation::new()
                     .flip_vertical()
                     .flip_horizontal(),
             )
-            .color_order(mipidsi::options::ColorOrder::Bgr)
+            .color_order(mipidsi::options::ColorOrder::Rgb)
             .reset_pin(lcd_reset_pin!($peripherals))
     }};
 }
--- src/boards/mod.rs
+++ src/boards/mod.rs
@@ -5,3 +5,7 @@ pub mod esp32s3box;
 #[cfg(feature = "esp32s3box3")]
 #[macro_use]
 pub mod esp32s3box3;
+
+#[cfg(feature = "m5stackcores3")]
+#[macro_use]
+pub mod m5stackcores3;
