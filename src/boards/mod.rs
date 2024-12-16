889e41f4f20cf759e57b1f66491a8f954b762572
Juraj Michalek <juraj.michalek@espressif.com>
2024-12-16 18:40:44 +0100

    add ESP32-S3-BOX-3

--- src/boards/mod.rs
+++ src/boards/mod.rs
@@ -1,5 +1,7 @@
+#[cfg(feature = "esp32s3box")]
 #[macro_use]
 pub mod esp32s3box;
 
-// pub mod esp32c6devkitc1;
-// pub mod m5stackcores3;
+#[cfg(feature = "esp32s3box3")]
+#[macro_use]
+pub mod esp32s3box3;
