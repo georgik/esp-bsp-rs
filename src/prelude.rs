537996e9ea2e03e4a89f8925fe774298416543cf
Juraj Michalek <juraj.michalek@espressif.com>
2024-12-17 06:47:11 +0100

    add custom_board

--- src/prelude.rs
+++ src/prelude.rs
@@ -16,3 +16,8 @@ pub use crate::boards::esp32s3box3::{
 pub use crate::boards::m5stackcores3::{
     i2c_init, lcd_backlight_init, lcd_display, lcd_display_interface, lcd_reset_pin, lcd_spi,
 };
+
+pub use crate::boards::custom_board::{
+    custom_lcd_spi, custom_lcd_display_interface, custom_lcd_display, custom_i2c_init,
+    custom_lcd_backlight, CustomBoardConfig,
+};
